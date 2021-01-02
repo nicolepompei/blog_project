a3102172751935b5eaf8b2d9af50841b2ca265c3
nicolepompei <nicole.lynn.pompei@gmail.com>
2021-01-02 15:32:22 -0500

    built jwt authentication security classes

--- src/main/java/com/blog/blog_project/security/jwt/JwtUtils.java
+++ src/main/java/com/blog/blog_project/security/jwt/JwtUtils.java
@@ -53,4 +53,5 @@ public class JwtUtils {
         }
         return false;
     }
+
 }
---
+++ src/main/java/com/blog/blog_project/security/services/UserDetailsImpl.java
@@ -0,0 +1,133 @@
+//package com.blog.blog_project.security.services;
+//
+//import com.blog.blog_project.entities.User;
+//import com.fasterxml.jackson.annotation.JsonIgnore;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.userdetails.UserDetails;
+//
+//import java.util.Collection;
+//import java.util.Collections;
+//import java.util.List;
+//import java.util.Objects;
+//import java.util.stream.Collectors;
+//
+///**
+// * Implementation of the UserDetails interface because we need to get more data
+// */
+//public class UserDetailsImpl  implements UserDetails {
+//
+//    private static final long serialVersionUID = 1L;
+//
+//    private Long id;
+//
+//    private String username;
+//
+//    private String email;
+//
+//    @JsonIgnore
+//    private String password;
+//
+//    private Collection<? extends GrantedAuthority> authorities;
+//
+//
+//    public UserDetailsImpl(Long id, String username, String email, String password, Collection<? extends GrantedAuthority> user) {
+//        this.id = id;
+//        this.username = username;
+//        this.email = email;
+//        this.password = password;
+//
+//    }
+//
+//    /**ONLY APPLIES TO ADDING FUTURE FUNCTIONALITY
+//     * Set<Role> needs to be converted to List<GrantedAuthority> to work with Spring Security and Authentication object
+//     */
+//    public static UserDetailsImpl build(User user){
+////        List<GrantedAuthority> authorities = user.getRoles().stream()
+////                .map(role -> new SimpleGrantedAuthority(role.getName().name()))
+////                .collect(Collectors.toList());
+//
+//        return new UserDetailsImpl(
+//                user.getId(),
+//                user.getUsername(),
+//                user.getEmail(),
+//                user.getPassword(),
+//                getAuthorities("USER"));
+//    }
+//
+//
+//    private static Collection<? extends GrantedAuthority> getAuthorities(String role){
+//        return Collections.singletonList(new SimpleGrantedAuthority(role));
+//    }
+//
+//
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        return authorities;
+//    }
+//
+//    @Override
+//    public String getPassword() {
+//        return null;
+//    }
+//
+//    @Override
+//    public String getUsername() {
+//        return null;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonExpired() {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonLocked() {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean isCredentialsNonExpired() {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean isEnabled() {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o)
+//            return true;
+//        if (o == null || getClass() != o.getClass())
+//            return false;
+//        UserDetailsImpl user = (UserDetailsImpl) o;
+//        return Objects.equals(id, user.id);
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//}
