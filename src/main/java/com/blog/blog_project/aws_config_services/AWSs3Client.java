f51cf439f3d764abb22ad5b8c7d9a0ccfea56f8b
Earl Atta-Fynn <Earl.Atta.Fynn@gmail.com>
2021-01-05 13:51:31 -0500

    Amazon s3 client configuration pt 2

--- src/main/java/com/blog/blog_project/aws_config_services/AWSs3Client.java
+++ src/main/java/com/blog/blog_project/aws_config_services/AWSs3Client.java
@@ -7,13 +7,26 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import org.hibernate.result.Output;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Date;
+import java.util.Optional;
 
 @Service
 public class AWSs3Client {
@@ -40,5 +53,44 @@ public class AWSs3Client {
                 .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
                 .withRegion(Regions.US_EAST_1)
                 .build();
+    }
+
+    //Split into multiple helper methods
+    private File convertMultipartToFile(MultipartFile file) throws IOException {
+        //Look at turning into optional.
+        Optional<MultipartFile> optionalFile = Optional.ofNullable(file);
+
+//            File newFile = new File(optionalFile.orElseThrow(IOException::new).getOriginalFilename());
+        assert file != null; //?Necessary?
+        File newFile = new File(file.getOriginalFilename());
+            FileOutputStream output = new FileOutputStream(newFile);
+            output.write(file.getBytes());
+            output.close();
+        return newFile;
+    }
+
+    //puts object in s3 with public read permissions
+    private void uploadToS3(String fileName, File file) {
+        s3Client.putObject
+                (new PutObjectRequest(bucketName, fileName, file)
+                .withCannedAcl(CannedAccessControlList.PublicRead));
+    }
+
+    private String generateFileName(MultipartFile multiPart) {
+        return new Date().getTime() + "-" + multiPart.getOriginalFilename()
+                .replace(" ", "_");
+    }
+
+    public String uploadFile(MultipartFile multipartFile) {
+        String fileUrl = "";
+
+        try{
+            File file = convertMultipartToFile(multipartFile);
+            String fileName = generateFileName(multipartFile);
+            fileUrl = endpointURL + fileName;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
