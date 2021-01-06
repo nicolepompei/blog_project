56fe3dca6019ab2d250a29563f195b05a2087048
nicolepompei <nicole.lynn.pompei@gmail.com>
2021-01-05 20:09:28 -0500

    added ant matchers in websecurity config to allow unauthenticated users to access end points for getting all the blog posts

--- src/main/java/com/blog/blog_project/security/WebSecurityConfig.java
+++ src/main/java/com/blog/blog_project/security/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import com.blog.blog_project.security.services.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -74,7 +75,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests().antMatchers("/api/auth/**").permitAll()
-                .antMatchers("/api/test/**").permitAll()
+                .antMatchers(HttpMethod.GET,"/posts").permitAll()
+                .antMatchers(HttpMethod.GET,"/posts/**").permitAll()
                 .anyRequest().authenticated();
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
--- src/main/java/com/blog/blog_project/security/jwt/AuthEntryPointJwt.java
+++ src/main/java/com/blog/blog_project/security/jwt/AuthEntryPointJwt.java
@@ -1,7 +1,6 @@
 package com.blog.blog_project.security.jwt;
 
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.bridge.MessageUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.AuthenticationException;
