21bdd3ca17fd8d1d33010e5b58017cd1f91dbf58
nicolepompei <nicole.lynn.pompei@gmail.com>
2021-01-08 23:12:46 -0500

    should create and save post test

--- src/test/java/com/blog/blog_project/services/BlogPostServiceTest.java
+++ src/test/java/com/blog/blog_project/services/BlogPostServiceTest.java
@@ -13,6 +13,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -31,6 +33,9 @@ class BlogPostServiceTest {
     @Mock private AuthService authService;
     @Mock private PostMapper postMapper;
 
+    @Captor
+    private ArgumentCaptor<BlogPost> postArgumentCaptor;
+
     private BlogPostService blogPostService;
 
     @BeforeEach
@@ -42,11 +47,9 @@ class BlogPostServiceTest {
 
     @Test
     @DisplayName("Find a blog post by an id")
-    void shouldFindPostById(){
+    public void shouldFindPostById(){
 
-        User pompy = new User(1L, "pompy","password", "pompy@email.com", LocalDateTime.now(), new ArrayList<>());
-
-        BlogPost post = new BlogPost(12L, "Myy blog post", LocalDateTime.now(), LocalDateTime.now(), "blurb", "full text", "wwww.image.com", new HashSet<>(), pompy);
+        BlogPost post = new BlogPost(12L, "Myy blog post", LocalDateTime.now(), LocalDateTime.now(), "blurb", "full text", "wwww.image.com", new HashSet<>(), null);
         PostResponse expectedPostResponse = new PostResponse(12L, "pompy", "Myy blog post", "www.image.com", "blurb", "full text", new HashSet<>());
 
         Mockito.when(blogPostRepository.findById(12L)).thenReturn(Optional.of(post));
@@ -58,4 +61,28 @@ class BlogPostServiceTest {
         Assertions.assertThat(actualPostResponse.getTitle()).isEqualTo(expectedPostResponse.getTitle());
 
     }
+
+
+    @Test
+    @DisplayName("Create and save a blog post")
+    public void shouldCreateAndSavePost(){
+        User currentUser = new User(1234L, "pompy", "password", "pompy@email.com", LocalDateTime.now(), new ArrayList<>());
+        BlogPost post = new BlogPost(12L, "Myy blog post", LocalDateTime.now(), LocalDateTime.now(), "blurb", "full text", "wwww.image.com", new HashSet<>(), null);
+        PostRequest postRequest = new PostRequest(12L, "pompy", "Myy blog post", "blurb","full text", "wwww.image.com", new HashSet<>());
+
+        Mockito.when(authService.getCurrentUser()).thenReturn(currentUser);
+//        Mockito.when(blogPostRepository.findById(12L)).thenReturn(Optional.of(post));
+        Mockito.when(postMapper.map(postRequest, currentUser)).thenReturn(post);
+
+        blogPostService.createBlogPost(postRequest);
+        //since create post does not return anything, we use verify to verify that a piece of code was executed
+
+        //capture the object that is passed to the method. assert if it matches our expectations or not with with ArgumentCaptor to capture
+        //arguments, mockito captures the obj so you have access to it after the method execution
+        Mockito.verify(blogPostRepository, Mockito.times(1)).save(postArgumentCaptor.capture());
+
+        Assertions.assertThat(postArgumentCaptor.getValue().getId()).isEqualTo(12L);
+        Assertions.assertThat(postArgumentCaptor.getValue().getTitle()).isEqualTo("Myy blog post");
+    }
+
 }
